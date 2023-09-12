35550ab7feae5243510ea664f79262ae75ee848d
GSUDHACS <144793416+GSUDHACS@users.noreply.github.com>
2023-09-12 15:58:20 +0530

    Add files via upload

---
+++ main (2).py
@@ -0,0 +1,11 @@
+  # Leapyear
+def isLeapyear(year):
+  if(year % 4 == 0 and year % 100 != 0)or year % 400 == 0:
+    return True
+  else:
+    return False
+year = 2012
+if isLeapyear(year):
+  print ("{} is a leapyear.". format (year))
+else:
+  print ("{} is not a leapyear.". format (year))
